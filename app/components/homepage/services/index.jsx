8e2c06b658990fdb636d0c460fc3f389347b58de
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-04-02 12:43:31 +0530

    Services done

--- app/components/homepage/services/index.jsx
+++ app/components/homepage/services/index.jsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
-import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
-import lottieFile from "/public/lottie/study.json";
 
-function Services({services}) {
+function Services({ services }) {
   return (
     <div
-      id="education"
+      id="services"
       className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
     >
       <Image
@@ -18,12 +16,12 @@ function Services({services}) {
       />
       <div className="flex justify-center -translate-y-[1px]">
         <div className="w-3/4">
-          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
+          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent w-full" />
         </div>
       </div>
 
       <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
+        <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
             Services
@@ -33,44 +31,41 @@ function Services({services}) {
       </div>
 
       <div className="py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
-          <div className="flex justify-center items-start">
-            <div className="w-3/4 h-3/4">
-              <AnimationLottie animationPath={lottieFile} />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex flex-col gap-6">
-              {services.slice(0,4).map((service, index) => (
-                <GlowCard
-                  key={service._id}
-                  identifier={`service-${service._id}`}
+        <div className="grid grid-cols-2 gap-6">
+          {services.slice(0,4).map((service, index) => (
+            <GlowCard key={service._id} identifier={`service-${service._id}`}>
+              <div className="p-3 relative text-white h-[18rem]">
+                <Image
+                  src="/blur-23.svg"
+                  alt="Hero"
+                  width={1080}
+                  height={200}
+                  className="absolute bottom-0 opacity-80"
+                />
+                <div className="flex justify-center">
+                  <p className="text-sm sm:text-sm text-[#16f2b3]">
+                    {service.name} {`(${service.charge})`}
+                  </p>
+                </div>
+                <div
+                  className={`flex flex-col md:flex-row ${
+                    index % 2 === 0 ? "justify-start" : "justify-end"
+                  } items-center gap-x-8 px-3 py-5`}
                 >
-                  <div className="p-3 relative text-white">
-                    <Image
-                      src="/blur-23.svg"
-                      alt="Hero"
-                      width={1080}
-                      height={200}
-                      className="absolute bottom-0 opacity-80"
-                    />
-                    <div className="flex justify-center">
-                      <p className="text-sm sm:text-sm text-[#16f2b3]">
-                        {service.name} {`(${service.charge})`}
-                      </p>
-                    </div>
-                    <div className={`flex flex-col md:flex-row ${index % 2 === 0 ? 'justify-start' : 'justify-end'} items-center gap-x-8 px-3 py-5`}>
-                      <Image src={service.image.url} width={200} height={200} className="hover:scale-125" alt="Service Image" />
-                      <p className="text-sm sm:text-base mt-4 text-center">
-                        {service.desc}
-                      </p>
-                    </div>
-                  </div>
-                </GlowCard>
-              ))}
-            </div>
-          </div>
+                  <Image
+                    src={service.image.url}
+                    width={200}
+                    height={200}
+                    className="rounded-md hover:scale-105"
+                    alt="Service Image"
+                  />
+                  <p className="text-sm sm:text-base mt-4 text-center">
+                    {service.desc}
+                  </p>
+                </div>
+              </div>
+            </GlowCard>
+          ))}
         </div>
       </div>
     </div>
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
             <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></a>
           </li>
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SERVICES</div></a>
+            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#services"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SERVICES</div></a>
           </li>
           <li>
             <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#blogs"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">TESTIMONIALS</div></a>
